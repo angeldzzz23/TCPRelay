8354291ce0cdddb181277da24c1290cfb0d97232
angel zambrano <angeldzambrano99@gmail.com>
2025-06-14 11:45:26 -0700

    added sample project to TCP Relay

---
+++ TCPRelay/TCPRelay/TCPRelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCPRelayApp.swift
+//  TCPRelay
+//
+//  Created by angel zambrano on 6/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TCPRelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
